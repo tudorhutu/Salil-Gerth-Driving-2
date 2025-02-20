28aae18b93ff9d08d4d9b6dd60b153a4efd3078d
Paraschiv Tudor <gparaschiv@gmail.com>
2025-02-20 17:06:30 +0200

    more

--- Scripts/outro.gd
+++ Scripts/outro.gd
@@ -4,6 +4,8 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$AnimationPlayer.play("outro")
+	$AnimationPlayer.seek(10)
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
